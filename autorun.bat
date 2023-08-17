520dee661e1c48dc07cf81eabd39cd6d005ef82b
Serhii Myshko <sergeiown@gmail.com>
2023-08-17 11:24:23 +0300

    add autorun module

---
+++ autorun.bat
@@ -0,0 +1,25 @@
+@echo off
+set "targetPath=%~dp0BackUpMaster.bat"
+set "iconPath=%~dp0BackUpMaster.ico"
+set "shortcutPath=%startupFolder%\BackUpMaster.lnk"
+set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+REM Elevate the script to administrator privileges
+whoami /groups | find "S-1-16-12288" > nul
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%0' -Verb RunAs"
+    exit /b
+)
+
+if not exist "%~dp0\BackUpMaster.lnk" (
+    REM Create a shortcut
+    powershell.exe -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%startupFolder%\BackUpMaster.lnk'); $Shortcut.TargetPath = '%targetPath%'; $Shortcut.IconLocation = '%iconPath%'; $Shortcut.Save()"
+    powershell.exe -Command "$WshShell = New-Object -ComObject WScript.Shell; $Shortcut = $WshShell.CreateShortcut('%~dp0\BackUpMaster.lnk'); $Shortcut.TargetPath = '%targetPath%'; $Shortcut.IconLocation = '%iconPath%'; $Shortcut.Save()"
+    exit /b
+) else (
+    REM Delete existing shortcut
+    del "%startupFolder%\BackUpMaster.lnk"
+    del "%~dp0\BackUpMaster.lnk"
+    exit /b
+)
+
--- write_config.bat
+++ write_config.bat
@@ -4,23 +4,35 @@ if not exist config.ini (
     REM Request data from the user
     :rewrite
     setlocal enabledelayedexpansion
+    
     set "culture=en-US"
     set "source_path="
     set "destination_path="
     set /a "compression_level=1"
     set "excluded_extensions="
-
+    set "number_of_copies="
+    set "startupFolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+        
+    :input_source_path
     cls & echo Path to the files you want to back up^:
     set "folderSelection="
     for /f "delims=" %%d in ('powershell -Command "$culture = [System.Globalization.CultureInfo]::CreateSpecificCulture('%culture%'); Add-Type -AssemblyName System.Windows.Forms; $f = New-Object Windows.Forms.FolderBrowserDialog; $f.Description = 'Path to the files you want to back up:'; $f.Language = $culture; $f.ShowDialog(); $f.SelectedPath"') do set "folderSelection=%%d"
     set "source_path=!folderSelection!"
+    if not exist "!source_path!" (
+        goto input_source_path
+    )
     cls & echo Path to the files you want to back up: !source_path!
     timeout /t 2 >nul
 
+
+    :input_destination_path
     cls & echo Path to the backup results^:
     set "folderSelection="
     for /f "delims=" %%d in ('powershell -Command "$culture = [System.Globalization.CultureInfo]::CreateSpecificCulture('%culture%'); Add-Type -AssemblyName System.Windows.Forms; $f = New-Object Windows.Forms.FolderBrowserDialog; $f.Description = 'Path to the backup results:'; $f.Language = $culture; $f.ShowDialog(); $f.SelectedPath"') do set "folderSelection=%%d"
     set "destination_path=!folderSelection!"
+        if not exist "!destination_path!" (
+        goto input_destination_path
+    )
     cls & echo Path to the backup results: !destination_path!
     timeout /t 2 >nul
 
@@ -57,18 +69,57 @@ if not exist config.ini (
         color 0C
         cls & echo An error occurred while writing to the config.ini file
         timeout /t 2 >nul
+        set "exitScript=1"
+        exit /b
     ) else (
         color 0A
         cls & echo Configuration data is successfully written to the config.ini file
         timeout /t 2 >nul
     )
+    
+    @REM Add and remove BackUpMaster autorun
+    color 07
+
+    if not exist "%~dp0\BackUpMaster.lnk" (
+        cls & choice /c YN /n /m "Do you want to turn on BackUpMaster autorun? (Y - Yes, N - No): "
+        if "!errorlevel!"=="1" (
+            call autorun.bat
+        ) else (
+            exit /b
+        )
+    ) else (
+        cls & choice /c YN /n /m "Do you want to disable BackUpMaster autorun? (Y - Yes, N - No): "
+        if "!errorlevel!"=="1" (
+            call autorun.bat
+        ) else (
+            exit /b
+        )
+    )
 
     endlocal
+
 ) else ( 
     REM Suggestion to overwrite the configuration if a file is available  
     setlocal enabledelayedexpansion
     cls & color 07
-    choice /c 12 /n /m "Create a new configuration? (1 - Yes, 2 - No): "
+    timeout /t 1 >nul
+
+    REM Reading data from the configuration file
+    echo Current configuration^:
+    echo.
+    for /f "usebackq tokens=1,2 delims==" %%i in ("config.ini") do (
+        if "%%i"=="source_path" echo Source path          : %%j
+        if "%%i"=="destination_path" echo Destination path     : %%j
+        if "%%i"=="compression_level" echo Compression level    : %%j
+        if "%%i"=="excluded_extensions"  echo Excluded extensions  : %%j
+        if "%%i"=="number_of_copies" echo Number of copies     : %%j
+    )
+    if exist "%~dp0\BackUpMaster.lnk" (
+        echo BackUpMaster autorun : On
+    ) else (echo BackUpMaster autorun : Off)
+    echo.
+
+    choice /c YN /n /m "Create a new configuration? (Y - Yes, N - No): "
     if "!errorlevel!"=="1" (
         endlocal
         goto :rewrite
