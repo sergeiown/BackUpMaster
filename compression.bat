743c8d5154a0525028a57f9ba7b9341c23485890
Serhii Myshko <sergeiown@gmail.com>
2023-08-24 23:19:40 +0300

    add the progress bar

--- compression.bat
+++ compression.bat
@@ -21,19 +21,36 @@ set "date_time=%DATE:~-4%.%DATE:~3,2%.%DATE:~0,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:
 set "backup_filename=backup_%date_time%.7z"
 
 @REM Executing compression
-cls & echo %msg_26% & echo.
+cls & echo.
+echo %msg_26% & echo.
 
 if "%excluded_extensions%" == "" (
-    "%zpath%7z.exe" a -t7z -mx=%compression_level% -r "%destination_path%\%backup_filename%" "%source_path%\*.*" > "%destination_path%\last_backup_log.txt" 2>&1
+    start "" /b "%zpath%7z.exe" a -t7z -mx=%compression_level% -r "%destination_path%\%backup_filename%" "%source_path%\*.*" > "%destination_path%\last_backup_log.txt" 2>&1
 ) else (
-    "%zpath%7z.exe" a -t7z -mx=%compression_level% -r -x!"%excluded_extensions%" "%destination_path%\%backup_filename%" "%source_path%\*.*" > "%destination_path%\last_backup_log.txt" 2>&1
+    start "" /b "%zpath%7z.exe" a -t7z -mx=%compression_level% -r -x!"%excluded_extensions%" "%destination_path%\%backup_filename%" "%source_path%\*.*" > "%destination_path%\last_backup_log.txt" 2>&1
 )
+
+@REM Displaying the progress bar
+setlocal enabledelayedexpansion
+:wait_loop
+tasklist /fi "imagename eq 7z.exe" | find /i "7z.exe" >nul
+if not errorlevel 1 (
+    set "progress=!progress!*"
+    cls & echo.
+    echo %msg_26%
+    echo.
+    echo !progress!
+    timeout /t 2 >nul
+    goto wait_loop
+)
+endlocal
+
 echo. >> "%destination_path%\last_backup_log.txt"
 
 findstr /c:"Everything is Ok" "%destination_path%\last_backup_log.txt" >nul
 if %errorlevel% equ 0 (
     color 0A
-    echo.
+    cls & echo.
     echo %msg_27% & echo %date_time% - %msg_27% "%destination_path%\%backup_filename%" >> "%destination_path%\%main_backup_log.txt"
     echo.
     timeout /t 2 >nul
@@ -55,6 +72,7 @@ if %errorlevel% equ 0 (
     endlocal
 ) else (
     color 0C
+    cls & echo.
     echo %msg_30% %destination_path%\last_backup_log.txt
     echo %date_time% - Backup failed     %destination_path%\%backup_filename% >> "%destination_path%\main_backup_log.txt"
     echo.
